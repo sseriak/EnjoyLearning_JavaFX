3c03e68110f333633976e76e5a4f630c10bdd79d
Sergei Seriak <sergeiseryak@gmail.com>
2024-11-20 22:50:40 +0100

    add scene switcher

--- src/main/java/com/example/enjoylearning/HelloController.java
+++ src/main/java/com/example/enjoylearning/HelloController.java
@@ -1,14 +1,35 @@
 package com.example.enjoylearning;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+    private Stage stage;
+    private Scene scene;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+
+    public void switchToSecondScene(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("second-scene.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Second scene!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToHelloScene(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello scene!");
+        stage.setScene(scene);
+        stage.show();
     }
 }
